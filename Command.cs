66e9f0e5fa4a23e6fb20075f411904af79fa4467
Gustavo H. Brunelli <gubrunelli@hotmail.com>
2020-09-12 19:55:05 -0300

    Add project files.

---
+++ Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CustomizacaoMoradias
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Hello World");
+            return Result.Succeeded;
+        }
+    }
+}
