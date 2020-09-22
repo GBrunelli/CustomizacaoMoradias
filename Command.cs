82a51db48c456dca873366548b4fae46b43aedcd
Gustavo H. Brunelli <gubrunelli@hotmail.com>
2020-09-22 14:42:14 -0300

    commit

--- Command.cs
+++ Command.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
 
 namespace CustomizacaoMoradias
 {
-    [Transaction(TransactionMode.Manual)]
-    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.ReadOnly)]
     public class Command : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Title", "Hello World");
+
+            UIDocument uiDocument = commandData.Application.ActiveUIDocument;
+
+            try
+            {
+                Reference pickedObj = uiDocument.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if (pickedObj != null)
+                {
+                    TaskDialog.Show("ElementId", pickedObj.ElementId.ToString());
+                }
+            
+            } 
+            catch(Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
             return Result.Succeeded;
         }
     }
