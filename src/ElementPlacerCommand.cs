a27e1d69d1c12987e10f8b37187f63e3c1888917
gubru <gubru@LAPTOP-UPBARD6F>
2021-03-27 18:12:10 -0300

    Implementado uma tab do plugin que aparecerá no Revit.

---
+++ src/ElementPlacerCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace CustomizacaoMoradias
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.NoCommandData)]
+    public class ElementPlacerCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                PlaceElementsApplication.thisApp = new PlaceElementsApplication();
+                PlaceElementsApplication.thisApp.ShowForm(commandData.Application);
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
--- src/PlaceElementsApplication.cs
+++ src/PlaceElementsApplication.cs
@@ -1,4 +1,12 @@
-﻿using Autodesk.Revit.UI;
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.UI;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Windows;
 
 namespace CustomizacaoMoradias
 {
@@ -10,22 +18,28 @@ namespace CustomizacaoMoradias
         // ModelessForm instance
         private PlaceElementsForm selectorForm;
 
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab("My Commands");
+            string path = Assembly.GetExecutingAssembly().Location;
+            PushButtonData elementPlacerButtonData = new PushButtonData("ElementPlacerButton", "Place Elements", path, "CustomizacaoMoradias.ElementPlacerCommand");
+            RibbonPanel elementPlacerRibbonPanel = application.CreateRibbonPanel("My Commands", "Commands");
+            PushButton elementPlacerButton = elementPlacerRibbonPanel.AddItem(elementPlacerButtonData) as PushButton;
+            elementPlacerButton.LargeImage = ImageSourceFromBitmap(Properties.Resources.floor_plan_32px);
+
+            selectorForm = null;   // no dialog needed yet; the command will bring it
+            thisApp = this;  // static access to this application instance
+            return Result.Succeeded;
+        }
+
         public Result OnShutdown(UIControlledApplication application)
         {
             if (selectorForm != null && selectorForm.Visible)
             {
                 selectorForm.Close();
             }
-
             return Result.Succeeded;
-        }
-
-        public Result OnStartup(UIControlledApplication application)
-        {
-            selectorForm = null;   // no dialog needed yet; the command will bring it
-            thisApp = this;  // static access to this application instance
-            return Result.Succeeded;
-        }
+        }       
 
         // The external command invokes this on the end-user's request
         public void ShowForm(UIApplication uiapp)
@@ -45,6 +59,19 @@ namespace CustomizacaoMoradias
                 PlaceElementsForm.form = selectorForm;
                 selectorForm.Show();
             }
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+        public static ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
         }
     }
 }
