5c00cb19663e27be970806b1d3c66eda5a16702d
Gustavo H Brunelli <gubrunelli@hotmail.com>
2021-05-10 18:50:15 -0300

    O ponto para a nova reta está sendo inserido na lista encadeada com sucesso.

--- Source/ElementPlacer.cs
+++ Source/ElementPlacer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -537,14 +538,13 @@ namespace CustomizacaoMoradias
             LinkedList<UV> points = new LinkedList<UV>();
             foreach(Curve curve in curveArray)
             {
-                XYZ p = curve.GetEndPoint(0);
-                UV point2D = new UV(p.X, p.Y);
+                UV point2D = ProjectInPlaneXY(curve.GetEndPoint(0));                           
                 points.AddLast(point2D);
             }
             return points;
         }
 
-        private double GetAngle(UV p0, UV p1, UV p2)
+        private double CalculatesAngle(UV p0, UV p1, UV p2)
         {
             UV vector1 = p1.Subtract(p0);
             UV vector2 = p2.Subtract(p1);
@@ -561,7 +561,7 @@ namespace CustomizacaoMoradias
             UV p0 = points.Last();
             UV p1 = points.First();
             UV p2 = points.First.Next.Value;
-            angle = GetAngle(p0, p1, p2);
+            angle = CalculatesAngle(p0, p1, p2);
             if (angle > Math.PI)
                 notches.Add(p1);
 
@@ -573,7 +573,7 @@ namespace CustomizacaoMoradias
                 p0 = node.Previous.Value;
                 p1 = node.Value;
                 p2 = node.Next.Value;
-                angle = GetAngle(p0, p1, p2);
+                angle = CalculatesAngle(p0, p1, p2);
                 if (angle > Math.PI)
                     notches.Add(p1);
             }
@@ -581,7 +581,7 @@ namespace CustomizacaoMoradias
             p0 = points.Last.Previous.Value;
             p1 = points.Last();
             p2 = points.First();
-            angle = GetAngle(p0, p1, p2);
+            angle = CalculatesAngle(p0, p1, p2);
             if (angle > Math.PI)
                 notches.Add(p1);
 
@@ -591,15 +591,177 @@ namespace CustomizacaoMoradias
 
         public List<CurveArray> GetConvexPerimeters(CurveArray curveArray)
         {
-            // get all points    
             LinkedList<UV> points = GetPoints(curveArray);
-
-            // get all notches
-            // https://stackoverflow.com/questions/12083480/finding-internal-angles-of-polygon
-            // Angle[i] =  Pi + ArcTan2(V[i] x V[i+1], V[i] * V[i+1])   
             List<UV> notches = GetNotches(points);
 
+            foreach(UV notche in notches)
+            {
+                EliminateNotch(notche, curveArray, ref points);
+            }
+
             return null;
+        }
+
+        private List<CurveArray> EliminateNotch(UV notche, CurveArray curveArray, ref LinkedList<UV> points)
+        {
+            XYZ notche3D = TranformIn3D(notche);
+            Line line1 = Line.CreateUnbound(notche3D, XYZ.BasisX);
+            Line line2 = Line.CreateUnbound(notche3D, XYZ.BasisY);
+
+            // get the posible curves for the new point
+            CurveArray posibleCurves = new CurveArray();
+            foreach(Curve curve in curveArray)
+            {
+                UV p0 = ProjectInPlaneXY(curve.GetEndPoint(0));
+                UV p1 = ProjectInPlaneXY(curve.GetEndPoint(1));
+                
+                // the curve will be appended if none of the
+                // end points is equal to the notche
+                if(!p0.Subtract(notche).IsZeroLength() && 
+                   !p1.Subtract(notche).IsZeroLength())
+                {
+                    posibleCurves.Append(curve);
+                }
+            }
+
+            // iterate for each possible curve, and if
+            // a intersection is found, the point will 
+            // added in the linked list
+            foreach(Curve curve in posibleCurves)
+            {
+                IntersectionResultArray resultArray;
+                var intersection = curve.Intersect(line1, out resultArray);
+                if (intersection == SetComparisonResult.Overlap)
+                {
+                    AddPointsInList(ref points, resultArray, curve);
+                    break;
+                }
+                else
+                {
+                    intersection = curve.Intersect(line2, out resultArray);
+                    if (intersection == SetComparisonResult.Overlap)
+                    {                       
+                        AddPointsInList(ref points, resultArray, curve);
+                        break;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        #region Order Points Clock Wise (Deprecated)
+        /*
+        class ClockWisePointComparer : IComparer
+        {
+            public static UV centroid;
+
+            public int Compare(object x, object y)
+            {
+                UV a = x as UV;
+                UV b = y as UV;
+
+                if (a.U - centroid.U >= 0 && b.U - centroid.U < 0)
+                    return -1;
+                if (a.U - centroid.U < 0 && b.U - centroid.U >= 0)
+                    return 1;
+                if (a.U - centroid.U == 0 && b.U - centroid.U == 0)
+                {
+                    if (a.V - centroid.V >= 0 || b.V - centroid.V >= 0)
+                        return a.V > b.V;
+                    return b.V > a.V;
+                }
+
+                // compute the cross product of vectors (center -> a) x (center -> b)
+                double det = (a.U - centroid.U) * (b.V - centroid.V) - (b.U - centroid.U) * (a.V - centroid.V);
+                if (det < 0)
+                    return true;
+                if (det > 0)
+                    return false;
+
+                // points a and b are on the same line from the center
+                // check which point is closer to the center
+                double d1 = (a.U - centroid.U) * (a.U - centroid.U) + (a.V- centroid.V) * (a.V - centroid.V);
+                double d2 = (b.U - centroid.U) * (b.U - centroid.U) + (b.V - centroid.V) * (b.V - centroid.V);
+                return d1 > d2;
+                
+            }
+
+            public static UV Compute2DPolygonCentroid(LinkedList<UV> points)
+            {
+                centroid = new UV(0, 0);
+                int vertexCount = points.Count();
+                UV[] vertices = new UV[vertexCount];
+                points.CopyTo(vertices, 0);               
+                double signedArea = 0.0;
+                double u0 = 0.0; // Current vertex X
+                double v0 = 0.0; // Current vertex Y
+                double u1 = 0.0; // Next vertex X
+                double v1 = 0.0; // Next vertex Y
+                double a = 0.0;  // Partial signed area
+
+                // For all vertices
+                for (int i = 0; i < vertexCount; i++)
+                {
+                    u0 = vertices[i].U;
+                    v0 = vertices[i].V;
+                    u1 = vertices[(i + 1) % vertexCount].U;
+                    v1 = vertices[(i + 1) % vertexCount].V;
+                    a = u0 * v1 - u1 * v0;
+                    signedArea += a;
+                    double tempU = (u0 + u1) * a;
+                    double tempV = (v0 + v1) * a;
+                    centroid.Add(new UV(tempU, tempV));
+                }
+
+                signedArea *= 0.5;
+                double newU = centroid.U / (6.0 * signedArea);
+                double newV = centroid.V / (6.0 * signedArea);
+                centroid = new UV(newU, newV);
+
+                return centroid;
+            }
+        }
+        */
+        #endregion
+
+
+        private static void AddPointsInList(ref LinkedList<UV> points, IntersectionResultArray resultArray, Curve curve)
+        {
+            UV p0 = ProjectInPlaneXY(curve.GetEndPoint(0));
+            var node = FindPoint(points, p0);
+            var iterator = resultArray.ForwardIterator();
+            iterator.Reset();
+            while (iterator.MoveNext())
+            {
+                IntersectionResult result = iterator.Current as IntersectionResult;
+                UV intersectionPoint = ProjectInPlaneXY(result.XYZPoint);
+                points.AddAfter(node, intersectionPoint);
+                
+            }
+        }
+
+        private static LinkedListNode<UV> FindPoint(LinkedList<UV> points, UV key)
+        {
+            var node = points.First;
+            while(node != null)
+            {
+                var point = node.Value;
+                if (point.U == key.U && point.V == key.V)
+                    return node;
+                node = node.Next;
+            }
+            return null;
+        }
+
+        private static UV ProjectInPlaneXY(XYZ xyz)
+        {
+            return new UV(xyz.X, xyz.Y);
+        }
+
+        private static XYZ TranformIn3D(UV uv)
+        {
+            return new XYZ(uv.U, uv.V, 0);
         }
 
         /// <summary>
